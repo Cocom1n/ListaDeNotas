5122a19fb83e45ed5cafb5ab848919cd45e051b8
Carrasco Candela E R <candenwnh@gmail.com>
2023-10-28 19:18:41 -0300

    se agrego el estado y elimina por id

--- src/componentes/ListaNotas.js
+++ src/componentes/ListaNotas.js
@@ -2,30 +2,49 @@ import React, { useState } from "react";
 import "./listaNotas.css";
 
 function ListaDeNotas() {
-    const [nota, setNota] = useState("");
+
     const [lista, setLista] = useState([]);
+    const [nota, setNota] = useState("");
+    const[checkbox, setCheckbox] = useState(false);
 
     const añadir = () => {
-        setLista([...lista, nota]);
+        if (checkbox) {
+            const idNota = Math.max(...lista.map((nota) => nota.id),0) +1;
+            const nuevaNota = {
+                id: idNota, 
+                texto: nota, 
+                estado: true
+            };
+            setLista([...lista, nuevaNota]);
+        }
     }
 
-    const borrar = (notaAEliminar) => {
-        const nuevaLista = lista.filter(nota => nota !== notaAEliminar);
+    const borrar = (idAEliminar) => {
+        const nuevaLista = lista.filter(nota => nota.id !== idAEliminar);
         setLista(nuevaLista);
+    }
+
+    const verificarCasilla = () => {
+        setCheckbox(!checkbox);
     }
 
     return (
 
         <>
-        <div class="contenedor">
+        <div className="contenedor">
             <h1>LISTA NOTAS</h1>
-            <input value={nota} onChange={(e) => setNota(e.target.value)}></input>
-            <button onClick={añadir}>Agregar nota</button>
+            <section className="cuadro">
+                <input value={nota} onChange={(e) => setNota(e.target.value)}></input>
+                <section className="botones">
+                    <input type="checkbox" checked={checkbox} onChange={verificarCasilla}/>
+                    <button onClick={añadir}>Agregar nota</button>
+                </section>
+            </section>
             <ul class="lista">
                 {lista.map((nota, index) => (
-                    <li key={index}>
-                        {nota}
-                        <button onClick={() => borrar(nota)}>Eliminar</button>
+                    <li key={nota.id}>
+                        {nota.id}- {nota.texto}
+                        <button onClick={() => borrar(nota.id)}>Eliminar</button>
                     </li>
                 ))}
             </ul>
